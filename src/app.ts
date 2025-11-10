49e36a91f22a74b91f4edc0b3b157c9b09ba33c0
bit-programmer <harsh.81928@gmail.com>
2025-11-10 18:59:35 +0530

    Added secure routes

--- src/app.ts
+++ src/app.ts
@@ -1,60 +1,117 @@
-import { Hono } from 'hono';
-import { db } from './core/db/index.js';
-import { sign, verify } from 'hono/jwt';
-import { JWT_SECRET } from './utils/constants.js';
-import { v4 as uuidv4 } from 'uuid';
-import { cors } from 'hono/cors';
+import { sql } from 'drizzle-orm'
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { sign, verify } from 'hono/jwt'
+import { db } from './core/db/index.js'
+import { JWT_SECRET } from './utils/constants.js'
 
-const app = new Hono();
+const app = new Hono()
 
 app.use('*', cors({
-    origin: "*"
-}));
+  origin: '*',
+}))
 
 app.get('/', (c) => {
-    return c.json({
-        message: 'Hello World',
-    });
+  return c.json({
+    message: 'Hello World',
+  })
 })
 
-app.post("/login", async (c) => {
-    const body = await c.req.json();
-    const response = await db.$client.execute(`select username, password from users where username = '${body.username}' and password = '${body.password}'`);
-    try {
-        const username = response.rows[0]['username']
-        const payload = {
-          sub: username,
-          exp: Math.floor(Date.now() / 1000) + 60 * 5,
-        }
-        const token = await sign(payload, JWT_SECRET);
-        return c.json({
-            token: token
-        });
-    } catch(e) {
-        return c.json({
-            token: null
-        }, 401);
+app.post('/secure/login', async (c) => {
+  const body = await c.req.json()
+  const response = await db.run(sql`select username, password from users where username = ${body.username} and password = ${body.password}`)
+  try {
+    const username = response.rows[0].username
+    const payload = {
+      sub: username,
+      exp: Math.floor(Date.now() / 1000) + 60 * 5,
     }
-
-})
-
-app.get("/generatetoken", async (c) => {
-    let tokenToVerify: string | undefined = c.req.query("token");
-    try {
-        await verify(tokenToVerify || "", JWT_SECRET);
-    } catch (e) {
-        return c.json({
-            message: "Invalid token",
-        }, 401);
-    }
-
-    const response = await fetch('https://glitch-operation.vercel.app/api/v1/contest-submission/1');
-    const { uniqueGeneratedCode } = await response.json();
-
+    const token = await sign(payload, JWT_SECRET)
     return c.json({
-        message: uniqueGeneratedCode,
-    }, 200);
-
+      username,
+      token,
+    }, { status: 201 })
+  }
+  catch (e) {
+    return c.json({
+      username: null,
+      token: null,
+    }, 401)
+  }
 })
 
-export default app;
+app.get('/secure/validtoken', async (c) => {
+  const tokenToVerify: string | undefined = c.req.query('token')
+  try {
+    await verify(tokenToVerify || '', JWT_SECRET)
+    return c.json({
+      isValidToken: true,
+    }, 200)
+  }
+  catch (e) {
+    return c.json({
+      isValidToken: false,
+    }, 200)
+  }
+})
+
+app.post('/login', async (c) => {
+  const body = await c.req.json()
+  const response = await db.$client.execute(`select username, password from users where username = '${body.username}' and password = '${body.password}'`)
+  try {
+    const username = response.rows[0].username
+    const payload = {
+      sub: username,
+      exp: Math.floor(Date.now() / 1000) + 60 * 5,
+    }
+    const token = await sign(payload, JWT_SECRET)
+    return c.json({
+      token,
+    })
+  }
+  catch (e) {
+    return c.json({
+      token: null,
+    }, 401)
+  }
+})
+
+app.get('/generatetoken', async (c) => {
+  const tokenToVerify: string | undefined = c.req.query('token')
+  try {
+    await verify(tokenToVerify || '', JWT_SECRET)
+  }
+  catch (e) {
+    return c.json({
+      message: 'Invalid token',
+    }, 401)
+  }
+
+  const response = await fetch('https://glitch-operation.vercel.app/api/v1/contest-submission/1')
+  const { uniqueGeneratedCode } = await response.json()
+
+  return c.json({
+    message: uniqueGeneratedCode,
+  }, 200)
+})
+
+app.get('/generatetoken/source-map-exploitation', async (c) => {
+  const tokenToVerify: string | undefined = c.req.query('token')
+  try {
+    await verify(tokenToVerify || '', JWT_SECRET)
+  }
+  catch (e) {
+    return c.json({
+      message: 'Invalid token',
+    }, 401)
+  }
+
+  const response = await fetch('https://glitch-operation.vercel.app/api/v1/contest-submission/4')
+  const { uniqueGeneratedCode } = await response.json()
+
+  return c.json({
+    message: uniqueGeneratedCode,
+  }, 200)
+})
+
+export default app
